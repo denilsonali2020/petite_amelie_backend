2bfd04d7e68417a47d4617d0a9aded372fd1fa94
denilsonali2020 <denilsontrochez32@gmail.com>
2026-06-24 23:22:11 -0600

    Mensaje descriptivo de tus cambios

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,27 @@
 import { CorsOptions } from "cors";
 
 //Permitir conexiones
-export const corsConfig: CorsOptions = {
+// export const corsConfig: CorsOptions = {
+//   origin: function (origin, callback) {
+//     const whitelist = [process.env.FRONTEND_URL];
+//     if (process.argv[2] === "--api") {
+//       whitelist.push(undefined);
+//     }
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Error de CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
+
+export const corsOptions: CorsOptions = {
   origin: function (origin, callback) {
-    const whitelist = [process.env.FRONTEND_URL];
-    if (process.argv[2] === "--api") {
-      whitelist.push(undefined);
-    }
-    if (whitelist.includes(origin)) {
+    if (origin === process.env.FRONTEND_URL) {
       callback(null, true);
     } else {
       callback(new Error("Error de CORS"));
     }
   },
-  credentials: true,
 };
