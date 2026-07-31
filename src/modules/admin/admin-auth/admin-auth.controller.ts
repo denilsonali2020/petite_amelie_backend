9b35e8f1c261919fdcb6519614945dea1f7faa15
denilsonali2020 <denilsontrochez32@gmail.com>
2026-07-30 19:21:28 -0600

    mejorando la navegacion de una pestaña a otra y manteniendo la sesion en produccion

--- src/modules/admin/admin-auth/admin-auth.controller.ts
+++ src/modules/admin/admin-auth/admin-auth.controller.ts
@@ -6,7 +6,7 @@ import { User } from "../../../generated/prisma/client.js";
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict" as const,
+  sameSite: 'lax' as const,
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días duracion del refresh token
 };
 
